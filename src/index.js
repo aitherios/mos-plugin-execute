0ac70371ac3dd498f81c2e55413a9b3470c9d18f
Renan Mendes Carvalho <aitherios@gmail.com>
2016-11-27 18:43:11 -0200

    Bugfix correct plugin registered name

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ module.exports = plugin
 const createExecuteRenderer = require('./create-execute-renderer')
 
 function plugin (mos, markdown) {
-  mos.scope.processOutput = createExecuteRenderer()
+  mos.scope.execute = createExecuteRenderer()
 }
 
 plugin.attributes = {
